2d571fde4578b86b20be834bbf98a93eeb5a8d69
Soren Frederiksen <SorenFrederiksen@fairfaxmedia.com.au>
2019-04-28 15:33:47 +0200

    Few minor additions

--- src/server/index.js
+++ src/server/index.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
 app.listen(process.env.PORT || 8500, () => {
   console.log(
     `[${new Date().toISOString()}]`,
-    chalk.blue(`App is running: 🌎 ${process.env.HOST || 'http://localhost'}:${process.env.PORT || 8500}`)
+    chalk.blue(`Weather widget is running 📺: ${process.env.HOST || 'http://localhost'}:${process.env.PORT || 8500}`)
   );
 });
 
